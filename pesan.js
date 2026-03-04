5b37cf55b70825d6f9558db2f549c4d1d58a7efe
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-04 11:14:32 +0700

    feat: menampilkan quotes yang sudah di tulis

---
+++ pesan.js
@@ -0,0 +1,10 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({ input, output });
+
+const Quotes = await rl.question('Quotes Hari Ini? ');
+
+console.log(` ${Quotes}`);
+
+rl.close();
