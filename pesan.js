866ddd9e2ec910dd276a4b3dfe36146844fb63e8
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-04 11:19:43 +0700

    feat : add 🐮cowsayjs

--- pesan.js
+++ pesan.js
@@ -1,3 +1,6 @@
+import { cowsay, cowthink, moo } from "cowsayjs";
+
+
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
@@ -5,6 +8,7 @@ const rl = readline.createInterface({ input, output });
 
 const Quotes = await rl.question('Quotes Hari Ini? ');
 
+console.log(cowsay(`will anyone read this?`));
 console.log(` ${Quotes}`);
 
 rl.close();
